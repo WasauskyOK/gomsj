098ee51b89f3fc2f3917f0dd13bb1b6f45e73463
wasauskyok <daniechoque159@gmail.com>
2020-06-11 18:07:02 -0500

    one  commit

---
+++ build/precache-manifest.f83d8a9151bfbd147d161c9a5ece44ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7f13fc569727419f5128b65bd5fe423",
+    "url": "/index.html"
+  },
+  {
+    "revision": "affd90960e7035fda3ff",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "8fdaaa9189f615cd4cc0",
+    "url": "/static/css/main.5093d950.chunk.css"
+  },
+  {
+    "revision": "affd90960e7035fda3ff",
+    "url": "/static/js/2.20c86676.chunk.js"
+  },
+  {
+    "revision": "8fdaaa9189f615cd4cc0",
+    "url": "/static/js/main.1610c5a6.chunk.js"
+  },
+  {
+    "revision": "47937746b50ed139ff4c",
+    "url": "/static/js/runtime-main.52075e16.js"
+  },
+  {
+    "revision": "0f93fa9cdbd888cb551b8c446631ac73",
+    "url": "/static/media/WhatsApp Mensaje Recibido.0f93fa9c.mp3"
+  }
+]);
